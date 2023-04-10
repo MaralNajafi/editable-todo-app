bffc77b88fc452da93e8a10a9c071074f00e16f5
Maral Najafi <marawlnajafi@gmail.com>
2023-04-10 15:37:19 +0330

    update: AppContainer address

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import AppContainer from "./components/Containers/AppContainer";
+import AppContainer from "./components/AppContainer/AppContainer";
 function App() {
   return <AppContainer />;
 }
